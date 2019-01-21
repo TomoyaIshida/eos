54dc343457b6f260a0d57feb8fc5b0b700eaa859
TomoyaIshida <tmyisd@icloud.com>
2019-01-21 17:28:26 +0900

    Update index

--- eos_addressbook/pages/index.vue
+++ eos_addressbook/pages/index.vue
@@ -4,8 +4,8 @@
       <span class="navbar-brand mb-0 h1">連絡先リスト</span>
       <span v-if='account' class="navbar-text">{{account.name}}@{{account.authority}}</span>
     </nav>
-    <b-alert :show='this.warning' variant="warning" dismissible>{{this.warning}}</b-alert>
-    <b-alert :show='this.info' variant="success" dismissible>{{this.info}}</b-alert>
+    <b-alert :show='this.warning !== null' variant="warning" dismissible>{{this.warning}}</b-alert>
+    <b-alert :show='this.info !== null' variant="success" dismissible>{{this.info}}</b-alert>
     <table class="table table-striped">
       <thead>
         <tr>
@@ -33,22 +33,25 @@
 
     <b-modal v-model="showModal" id="contactModal" :title="modalTitle" centered>
       <b-form>
-        <b-form-group label=" 名前" label-for="name">
+        <b-form-group label="名前" label-for="name">
           <b-form-input type="text" v-model="modalContact.name" :state="!$v.modalContact.name.$invalid" aria-describedby="nameFeedback"></b-form-input>
-          <b-form-invalid-feedback id="nameFeedback">名前は 2-10 文字で入力してください</b-form-invalid-feedback>
+          <b-form-invalid-feedback id="nameFeedback">名前は 2-10 文字で入力してください</b-form-invalid-feedback>
         </b-form-group>
-        <b-form-group label=" 住所" label-for="address">
-          <b-form-input type="text" v-model="modalContact.address" :state="!$v.modalContact.address.$invalid" aria-describedby="addressFeedback"></b-form-input> <b-form-invalid-feedback id="addressFeedback">住所は 4-20 文字で入力してください</b-form-invalid-feedback>
+        <b-form-group label="住所" label-for="address">
+          <b-form-input type="text" v-model="modalContact.address" :state="!$v.modalContact.address.$invalid" aria-describedby="addressFeedback"></b-form-input>
+          <b-form-invalid-feedback id="addressFeedback">住所は 4-20 文字で入力してください</b-form-invalid-feedback>
         </b-form-group>
-        <b-form-group label=" 電話番号" label-for="tel">
-          <b-form-input type="text" v-model="modalContact.tel" :state="!$v.modalContact.tel.$invalid" aria-describedby="telFeedback"></b-form-input>
-          <b-form-invalid-feedback id="telFeedback">電話番号は 10-13 文字で入力してください</b-form-invalid-feedback>
+        <b-form-group label="電話番号" label-for="tel">
+          <b-form-input type="text" v-model="modalContact.tel" :state="!$v.modalContact.tel.$invalid" aria-describedby="telFeedback" ></b-form-input>
+          <b-form-invalid-feedback id="telFeedback">電話番号は 10-13 文字で入力してください</b-form-invalid-feedback>
         </b-form-group>
       </b-form>
 
       <div slot="modal-footer" class="w-100">
         <div class="float-right">
-          <b-btn size="sm" variant="default" @click="showModal = false">キャンセル</b-btn>
+          <b-btn size="sm" variant="default" @click="showModal = false">
+            キャンセル
+          </b-btn>
           <b-btn size="sm" class="ml-1" :variant="modalButtonClass" @click="saveContact" :disabled="$v.modalContact.$invalid">
             {{modalButtonText}}
           </b-btn>
@@ -60,39 +63,39 @@
 
 <script>
 import Vue from 'vue'
+import { validationMixin } from "vuelidate"
+import { required, minLength, maxLength } from "vuelidate/lib/validators"
 import Eos from 'eosjs'
 import ScatterJS from 'scatter-js/dist/scatter.esm'
-import { validationMixin } from "vuelidate"
-import { required, minLength, maxLength }from "vuelidate/lib/validators"
-
 const CONTRACT_ACCOUNT = 'eosbookaddrs'
 export default {
   data () {
     return {
       eos: null,
-      account: null,
       eosContract: null,
+      account: null,
       info: null,
       warning: null,
       modalContact: {},
       showModal: false,
       contacts: [],
     }
-  }, mixins: [
+  },
+  mixins: [
     validationMixin
   ],
   computed: {
     idExists () {
-      return this.modalContact && this.modalContact.id
+      return this.modalContact && this.modalContact.id >= 0
     },
     modalTitle () {
-      return this.idExists ? ' 連絡先編集' : ' 連絡先新規登録'
+      return this.idExists ? '連絡先編集' : '連絡先新規登録'
     },
     modalButtonClass () {
       return 'outline-' + (this.idExists ? 'success' : 'primary')
     },
     modalButtonText () {
-      return this.idExists ? ' 更新する' : ' 登録する'
+      return this.idExists ? '更新する' : '登録する'
     },
   },
   validations: {
@@ -105,7 +108,8 @@ export default {
       address: {
         required,
         minLength: minLength(4),
-        maxLength: maxLength(20) },
+        maxLength: maxLength(20)
+      },
       tel: {
         required,
         minLength: minLength(10),
@@ -116,33 +120,29 @@ export default {
   async asyncData (context) {
     const network = {
       blockchain: 'eos',
-      protocol: 'http',
-      host: '0.0.0.0',
-      port: 7777,
-      chainId: 'cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7dc4f',
+      protocol: 'https',
+      host: 'api-kylin.eoslaomao.com',
+      port: 443,
+      chainId: '5fff1dae8dc8e2fc4d5b23b2c7665c97f9e9d8edf2b6485a86ba311c25639191'
     }
-
-    const connected = await ScatterJS.scatter.connect("EOS Addressbook")
+    const connected = await ScatterJS.scatter.connect("eos-kylin-addressbook")
     if(!connected) return false;
-
     const scatter = ScatterJS.scatter;
     const requiredFields = { accounts: [network] };
-
     await scatter.getIdentity(requiredFields);
     const account = scatter.identity.accounts.find(x => x.blockchain === 'eos');
     const eosOptions = { expireInSeconds: 60 };
     const eos = scatter.eos(network, Eos, eosOptions);
     window.scatter = null;
-
-    const result = await eos.getTableRows(true, 'addressbook', 'bob', 'people')
-
-    return { eos, account, contacts: result.rows }
-  },
-  asyncrefreshContracts(){
-    const result = await this.eos.getTableRows(true, CONTRACT_ACCOUNT, CONTRACT_ACCOUNT,'people')
-    this.contacts = result.rows
+    const result = await eos.getTableRows(true, CONTRACT_ACCOUNT, CONTRACT_ACCOUNT, 'people')
+    const eosContract = await eos.contract(CONTRACT_ACCOUNT)
+    return { eos, eosContract, account, contacts: result.rows }
   },
   methods: {
+    async refreshContracts () {
+      const result = await this.eos.getTableRows(true, CONTRACT_ACCOUNT, CONTRACT_ACCOUNT, 'people')
+      this.contacts = result.rows
+    },
     newContact () {
       this.modalContact = {}
       this.showModal = true
@@ -152,22 +152,22 @@ export default {
       this.showModal = true
     },
     deleteContact (contact) {
-      if (confirm(' 連絡先を削除します。よろしいですか?')){
+      if (confirm('連絡先を削除します。よろしいですか？')) {
         this.warning = null
         this.info = null
-
         this.eosContract.destroy(
           this.account.name, contact.id,
           { authorization: this.account.name }
         ).then(result => {
-          this.warning = ' トランザクションを送信しました'
+          this.warning = 'トランザクションを送信しました'
         }).then(async () => {
-          this.info = ' 連絡先を削除しました'
+          this.refreshContracts()
+          this.info = '連絡先を削除しました'
         })
       }
     },
     saveContact () {
-      if (this.idExists){
+      if (this.idExists) {
         this.updateContact()
       } else {
         this.createContact()
@@ -177,28 +177,29 @@ export default {
     createContact () {
       this.warning = null
       this.info = null
-
       this.eosContract.create(
         this.account.name, this.modalContact.name, this.modalContact.address, this.modalContact.tel,
         { authorization: this.account.name }
       ).then(result => {
-        this.warning = ' トランザクションを送信しました'
+        this.warning = 'トランザクションを送信しました'
       }).then(async () => {
-        this.info = ' 連絡先を登録しました'
+        this.refreshContracts()
+        this.info = '連絡先を登録しました'
       })
     },
-    updateContact (contact) {
+    updateContact () {
       this.warning = null
       this.info = null
-
       this.eosContract.update(
-        this.account.name, this.modalContact.id, this.modalContact.name, this.modalContact.address,this.modalContact.tel,
+        this.account.name, this.modalContact.id, this.modalContact.name, this.modalContact.address, this.modalContact.tel,
         { authorization: this.account.name }
       ).then(result => {
-        this.warning = ' トランザクションを送信しました' }).then(async () => {
-        this.info = ' 連絡先を更新しました'
+        this.warning = 'トランザクションを送信しました'
+      }).then(async () => {
+        this.refreshContracts()
+        this.info = '連絡先を更新しました'
       })
     }
-  }
+  },
 }
 </script>
